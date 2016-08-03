fd59ab957956a25969b1a65d44a0c47871ac5548
yichiang <yi_chiang@outlook.com>
2016-08-03 14:09:27 -0700

    add consider two different num

--- test.js
+++ test.js
@@ -8,17 +8,51 @@ console.log(primeArr);
   for (var i = 0; i <=primeArr.length; i++)
   {
       for (var j = 0; j < arr[1]; j++) {
+        var tempOne=Math.pow(primeArr[i], j);
+        var tempTwo=Math.pow(primeArr[i],j+1);
         if(Math.pow(primeArr[i], j)<=arr[1]&&Math.pow(primeArr[i],j+1)>=arr[1])
         {
-          
+          var isMulity=false;
+          for (var k = arr[0]; k <=arr[1]; k++) {
+            if(k%tempOne==0)
+            {
+              console.log(Math.pow(primeArr[i], j));
+              isMulity=true;
+              resultPower.push(Math.pow(primeArr[i], j));
+              break;
+            }
 
-          console.log(primeArr[i]+" "+j);
-          resultPower.push(Math.pow(primeArr[i], j));
+          }
+
+          if(isMulity==false)
+          {
+            var temp=0;
+              var stop=false;
+              for (var z = j-1; z >= 0; z--) {
+                for (var g = arr[0]; g <arr[1]; g++) {
+                  console.log("g is"+ g+"z index is"+z+"Prime is "+primeArr[i] +"Consier num"+Math.pow(primeArr[i], z));
+                if(g%Math.pow(primeArr[i], z)===0)
+                {
+                  resultPower.push(Math.pow(primeArr[i], z));
+                  stop=true;
+                  break;
+                }
+              }
+              if(stop)
+              {
+                break;
+              }
+
+            }
+
+          }
+              // resultPower.push(Math.pow(primeArr[i], j));
+
         }
       }
 
   }
-
+  console.log(resultPower);
   return mulityAllitem(resultPower);
 }
 
